ef478b650b3697558f08dfce67ccebb700ce8c4f
Amritesh Pandey <smartwolfy99@gmail.com>
2025-07-24 19:01:30 +0530

    Update pdf_parser.py

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -32,7 +32,7 @@ def extract_text_from_pdf(uploaded_file):
     )
 
     response = client.chat.complete(
-        model="mistral-large-2411",
+        model="open-mistral-nemo",
         messages=[
             {
                 "role": "user",
